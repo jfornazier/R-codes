0c0bc9a058a1cd03dd6a4c0844a9e1430e8b61cd
jfornazier <jfornazier@usp.br>
2020-11-07 19:02:31 -0300

    testando funções

---
+++ importing-qza-files-qiime2.R
@@ -0,0 +1,4 @@
+ps1 <- qza_to_phyloseq(features = "feature-table-agua-petro.qza",
+                       tree = "rooted-tree.qza",
+                       taxonomy = "taxonomy-silva138.qza", 
+                       metadata = "map-file.txt")
