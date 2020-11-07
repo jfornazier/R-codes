ae315eec013080019ec9aefbfae1bf40aaa598c0
jfornazier <30935817+jfornazier@users.noreply.github.com>
2020-11-07 19:06:05 -0300

    Delete importing-qza-files-qiime2.R

--- importing-qza-files-qiime2.R
+++
@@ -1,4 +0,0 @@
-ps1 <- qza_to_phyloseq(features = "feature-table-agua-petro.qza",
-                       tree = "rooted-tree.qza",
-                       taxonomy = "taxonomy-silva138.qza", 
-                       metadata = "map-file.txt")
